75b240ee6a8f202066f6a8b110c38a06864e0171
Facu91Cufre <facundocufre91@gmail.com>
2024-05-09 14:59:37 -0300

    added error message

--- app.js
+++ app.js
@@ -4,12 +4,24 @@ const qrImage = document.getElementById("qr-image");
 const imgBox = document.querySelector(".img-box");
 
 const generateQR = () => {
+  const existingError = imgBox.querySelector("p");
+  if (existingError) {
+    imgBox.removeChild(existingError);
+  }
   if (userInput.value.length > 0) {
     let qrUrl =
       "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
       userInput.value;
     qrImage.src = qrUrl;
     imgBox.classList.add("show-image");
+    setTimeout(resetState, 7000);
+  } else {
+    let error = document.createElement("p");
+    error.innerText = "Por favor ingresá un valor.";
+    error.classList.add("error");
+    imgBox.appendChild(error);
+    imgBox.classList.add("show-image");
+    qrImage.style.display = "none";
     setTimeout(resetState, 7000);
   }
 };
